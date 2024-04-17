d1eb1f34e51fd02670fe2be38f8846bd6172c953
Zhiwei Lin <zhiwei.lin1@estudiantat.upc.edu>
2024-04-18 01:30:14 +0200

    settings

--- proyectoasw/proyectoasw/settings.py
+++ proyectoasw/proyectoasw/settings.py
@@ -9,20 +9,20 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Hay que modificarla
-SECRET_KEY = 'django-insecure-bnasfw4=d*%oeai)(dab$5@+7)h6!8v1r@+m=d@69u&$3iqc@&'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "True"
 
 # Se usa al querer hacer deploy
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -101,6 +101,8 @@ DATABASES = {
     }
 }
 
+database_url = os.environ.get("DATABASE_URL")
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
